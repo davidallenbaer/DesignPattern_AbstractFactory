2d986277cb3ecdbc23fbd13cafd46055464a1444
davidallenbaer <davidallenbaer@gmail.com>
2021-05-18 21:57:57 -0400

    Add project files.

---
+++ DesignPattern_AbstractFactory/Octopus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_AbstractFactory
+{
+    public class Octopus : IAnimal
+    {
+        public string speak()
+        {
+            return "SQUAWCK";
+        }
+    }
+}
